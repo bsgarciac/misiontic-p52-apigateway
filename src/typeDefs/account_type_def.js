2c21d1d651dbafc5321c703e6e29338a85144ad2
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-24 19:48:00 -0500

    Account Type Def

---
+++ src/typeDefs/account_type_def.js
@@ -0,0 +1,15 @@
+const { gql } = require('apollo-server');
+
+const accountTypeDefs = gql` 
+    type Account{
+        username: String!
+        balance: Int!
+        lastChange: String!
+    }
+
+    type Query{
+        accountByUsername(username: String!): Account
+    }
+`;
+
+module.exports = accountTypeDefs;
