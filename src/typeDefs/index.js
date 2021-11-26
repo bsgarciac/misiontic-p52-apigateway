ad1c3226461e2ccdd8d254bb19ccd136c9510938
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-25 19:24:00 -0500

    Login type def

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,5 +1,6 @@
 const accountTypeDefs = require('./account_type_def');
+const authTypeDefs = require('./auth_type_def')
 
-const schemasArray = [accountTypeDefs];
+const schemasArray = [accountTypeDefs, authTypeDefs];
 
 module.exports = schemasArray;
