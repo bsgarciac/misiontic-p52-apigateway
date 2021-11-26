38efcca16b3f57689ed29f1ac20bfbd2ca8bef5c
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-25 19:38:58 -0500

    Login resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,7 +1,8 @@
 const accountResolver = require('./account_resolvers');
+const authResolver = require('./auth_resolvers');
 
 const lodash = require('lodash');
 
-const resolvers = lodash.merge(accountResolver);
+const resolvers = lodash.merge(accountResolver, authResolver);
 
 module.exports = resolvers;
