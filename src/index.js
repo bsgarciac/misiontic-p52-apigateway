5674c91c9359d7a56f46533ece89688d162dcb92
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-25 20:42:00 -0500

    context in server

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers')
 const AccountAPI = require('./dataSources/account_api')
-const AuthAPI = require('./dataSources/auth_api')
+const AuthAPI = require('./dataSources/auth_api');
+const authentication = require('./utils/authentication');
 
 const server = new ApolloServer({
+    context: authentication,
     typeDefs,
     resolvers,
     dataSources: () => ({
