9d4ef3e43e2973424afcbb2def7dce5f522d3496
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-25 20:16:00 -0500

    signup resolver

--- src/resolvers/auth_resolvers.js
+++ src/resolvers/auth_resolvers.js
@@ -2,6 +2,22 @@ const authResolver = {
     Mutation: {
         login: async (_, { credentials }, { dataSources }) => {
             return await dataSources.authAPI.loginRequest(credentials);
+        },
+        signup: async (_, { signupData }, { dataSources }) => {
+            // Creating Account
+            const accountData = {
+                username: signupData.username,
+                balance: signupData.balance,
+                lastChange: (new Date()).toISOString()
+            }
+            await dataSources.accountAPI.createAccount(accountData);
+            // Creating User
+            const userData = {
+                username: signupData.username,
+                password1: signupData.password1,
+                password2: signupData.password2
+            }
+            return await dataSources.authAPI.createUser(userData);
         }
     }
 }
