935bcca3b8732891fde73cf839afb0b577fa87c6
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-29 19:50:52 -0500

    transaction typedefs

---
+++ src/typeDefs/transaction_type_def.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server');
+
+const transactionTypeDefs = gql`
+    type Transaction{
+        id: String!
+        usernameOrigin: String!
+        usernameDestiny: String!
+        value: Int
+        date: String
+    }
+
+    input TransactionInput{
+        usernameOrigin: String!
+        usernameDestiny: String!
+        value: Int
+    }
+
+    extend type Query {
+        transactionByUsername(username: String!): [Transaction]
+    }
+
+    extend type Mutation{
+        createTransaction(transaction: TransactionInput!): Transaction
+    }
+`;
